1f80e8a6e12c8e7ccc013cca8fd676cb3d1384b7
FasyaNFauzziyah44 <78576654+fasya44@users.noreply.github.com>
2021-07-25 21:35:19 +0700

    Add files via upload

---
+++ boolean.h
@@ -0,0 +1,14 @@
+/*---------------------------------------------
+
+	Program		: boolean.h
+	Deskripsi	: Header file boolean 
+	Author     : Ade Chandra Nugraha
+
+---------------------------------------------*/
+
+#ifndef boolean_H
+#define boolean_H
+#define true 1
+#define false 0
+#define boolean unsigned char
+#endif
