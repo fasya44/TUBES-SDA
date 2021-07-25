9199eb6429a4bd052b8627ae9c0cceb442b7dc99
fasya44 <fasya.nur.tif420polban.ac.id>
2021-07-25 23:00:11 +0700

    Add header.h pt2

--- header.h
+++ header.h
@@ -34,3 +34,23 @@ void Push (Stack *S, infotype X);
 /* I.S. S mungkin kosong, tabel penampung elemen stack TTDAK penuh */
 /* F.S. X menjadi TOP yang baru,TOP bertambah 1 */
 /**** Menqhapus sebuah elemen Stack *****/
+
+void Pop (Stack *S, infotype *X);
+/* Menghapus X dari Stack S. */
+/* I.S. S tidak mungkin kosong */
+/* F.S. X adalah nilai elemen TOP yang lama, TOP berkurang l */
+
+void displaylevel(int level);
+/* Menampilan level X dari Stack S. */
+/* I.S. S tidak mungkin kosong */
+/* F.S. X adalah nilai elemen level */
+
+void display();
+/* Menampilan function displaylevel yang telah dipanggil */
+/* I.S. S tidak mungkin kosong */
+/* F.S. X adalah nilai elemen level */
+
+int gameWinCondition();
+/* Menampilan X dari Stack S sesuai dengan aturan main. */
+/* I.S. S tidak mungkin kosong */
+/* F.S. X adalah nilai elemen TOP */
